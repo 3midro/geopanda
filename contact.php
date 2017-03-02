12a2da7582d8dc7125c69c872c4ba3563ef14a39
Unknown <3midro@gmail.com>
2017-03-02 14:23:21 -0600

    formulario de contacto
    Ya envia 2 correos, uno para el equipo de soporte y otro para el usuario, notificandole que pronto nos pondremos en contacto con ellos, ambos emails tienen estilos y se pueden modificar posteriormente de acuerdo a las necesidades

--- contact.php
+++ contact.php
@@ -1,22 +1,49 @@
 <?php
     include("php/PHPMailerEasy.php");
-    $n = (isset($_POST["name"]))?$_POST['name']:'Edd';
-    $to = (isset($_POST["email"]))?$_POST['email']:'3midro@gmail.com';
-    $p = (isset($_POST["phone"]))?$_POST['phone']:'4492599033';
-    $m = (isset($_POST["comments"]))?$_POST['comments']:'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo';
+    $uName = (isset($_POST["name"]))?$_POST['name']:'Edd';
+    $uMail = (isset($_POST["email"]))?$_POST['email']:'3midro@gmail.com';
+    $uPhone = (isset($_POST["phone"]))?$_POST['phone']:'4492599033';
+    $uMsg = (isset($_POST["comments"]))?$_POST['comments']:'El punto de usar Lorem Ipsum es que tiene una distribucion mas o menos normal de las letras, al contrario de usar textos como por ejemplo Contenido aqui, contenido aqui. Estos textos hacen parecerlo un espanol que se puede leer. Muchos paquetes de autoedición y editores de paginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una busqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo.';
    
+
+    $send = PHPMailerEasy::BothEmail($uName, $uMail, $uPhone, $uMsg);
+    if($send == "ok") {
+      echo "Message was sent successfully";
+    } else {
+      echo "Can't send message";
+         echo $send;
+    }
+    exit();
+
+
     
     //email respuesta para el usuario
     $text = file_get_contents('emailTemplate/user.html');
     $text = str_replace('[NAME]', $n, $text);
     $text = str_replace('[EMAIL]', $to, $text);
-    echo $text;
     $subject= "Geopanda contact";
-    $send = PHPMailerEasy::Email($to, $subject, $text);
+    $setFromA = 'contacto@geopanda.com.mx';
+    $setFromB = 'Geopanda Team';
+    //$reply = array ('contacto@geopanda.com.mx', 'Geopanda Team');
+    $send = PHPMailerEasy::Email($to, $subject, $text, $setFromA, $setFromB);
+    
+
+    //email para el soporte tecnico
+    $text = file_get_contents('emailTemplate/support.html');
+    $text = str_replace('[NAME]', $n, $text);
+    $text = str_replace('[EMAIL]', $to, $text);
+    $text = str_replace('[PHONE]', $p, $text);
+    $text = str_replace('[MESSAGE]', $m, $text);
+    $subject= "Email from ".$n;
+    $setFromA = $m;
+    $setFromB = $n;
+    $to = 'contacto@geopanda.com.mx';
+    $send = PHPMailerEasy::Email($to, $subject, $text, $setFromA, $setFromB);
     if($send == "ok") {
       echo "Message was sent successfully";
     } else {
-      echo "Can't send message";
+      echo "Can't send message<br>";
+       
     }
 
 ?>
--- php/PHPMailerEasy.php
+++ php/PHPMailerEasy.php
@@ -1,6 +1,6 @@
 <?php
 class PHPMailerEasy {
-    public static function Email($to, $subject, $msg) {
+    public static function Email($to, $subject, $msg, $setFromA, $setFromB) {
         if (!class_exists('PHPMailer')) {
             include 'PHPMailerAutoload.php'; //URL to your PHPMailer Autoload
         }
@@ -21,8 +21,9 @@ class PHPMailerEasy {
         $mail->SMTPAuth = true; //False if not SMTP
         $mail->Username = "contacto@geopanda.com.mx";
         $mail->Password = "rZxMg}uaU#gN";
-        $mail->setFrom('contacto@geopanda.com.mx', 'Geopanda');
-        $mail->addReplyTo('contacto@geopanda.com.mx', 'Geopanda Team');
+        $mail->setFrom($setFromA, $setFromB);
+        /*$mail->addReplyTo('contacto@geopanda.com.mx', 'Geopanda Team');*/
+        $mail->addReplyTo($reply);
         $mail->addAddress($to);
         $mail->Subject = $subject;
         //$mail->msgHTML(utf8_decode(nl2br($msg)), dirname(__FILE__));
@@ -32,5 +33,84 @@ class PHPMailerEasy {
         } else {
             return "ok";
         }
+    }
+    
+    public static function BothEmail($uName,$uMail,$uPhone,$uMsg){
+         if (!class_exists('PHPMailer')) {
+            include 'PHPMailerAutoload.php'; //URL to your PHPMailer Autoload
+        }
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->SMTPDebug = false;
+        $mail->Debugoutput = 'html';
+        $mail->Host = "mail.geopanda.com.mx"; //Host of mail
+        $mail->Port = 587; //Port of connection
+        $mail->SMTPSecure = 'tls'; //Type of connection
+        $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+        $mail->SMTPAuth = true; //False if not SMTP
+        $mail->Username = "contacto@geopanda.com.mx";
+        $mail->Password = "rZxMg}uaU#gN";
+        
+         $mailu = new PHPMailer;
+        $mailu->isSMTP();
+        $mailu->SMTPDebug = false;
+        $mailu->Debugoutput = 'html';
+        $mailu->Host = "mail.geopanda.com.mx"; //Host of mail
+        $mailu->Port = 587; //Port of connection
+        $mailu->SMTPSecure = 'tls'; //Type of connection
+        $mailu->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+        $mailu->SMTPAuth = true; //False if not SMTP
+        $mailu->Username = "contacto@geopanda.com.mx";
+        $mailu->Password = "rZxMg}uaU#gN";
+        
+        
+        
+        
+        //mail soporte
+        $mail->setFrom($uMail, $uName);
+        $mail->addReplyTo($uMail, 'Dear '.$uName);
+        $mail->addAddress('contacto@geopanda.com.mx');
+        $mail->Subject = 'Email from '.$uName;
+        $htmlEmail = file_get_contents('emailTemplate/support.html');
+        $htmlEmail = str_replace('[NAME]', $uName, $htmlEmail);
+        $htmlEmail = str_replace('[EMAIL]', $uMail, $htmlEmail);
+        $htmlEmail = str_replace('[PHONE]', $uPhone, $htmlEmail);
+        $htmlEmail = str_replace('[MESSAGE]', $uMsg, $htmlEmail);
+        $mail->msgHTML($htmlEmail);
+        if (!$mail->send()) {
+            return "No se envio el de soporte";
+        } else {
+            //return "ok";
+            //mail usuario
+            $mailu->setFrom('contacto@geopanda.com.mx', 'Geopanda contact');
+            $mailu->addReplyTo('contacto@geopanda.com.mx', 'Awsome Geopanda SUpport team');
+            $mailu->addAddress($uMail);
+            $mailu->Subject = 'Hi dear '.$uName;
+            $htmlEmailu = file_get_contents('emailTemplate/user.html');
+            $htmlEmailu = str_replace('[NAME]', $uName, $htmlEmailu);
+            $htmlEmailu = str_replace('[EMAIL]', $uMail, $htmlEmailu);
+            
+            $mailu->msgHTML($htmlEmailu);
+
+            if (!$mailu->send()) {
+                return "No se envio el de usuario";
+            } else {
+                return "ok";
+            }
+        }
+        
+        
     }
 }
